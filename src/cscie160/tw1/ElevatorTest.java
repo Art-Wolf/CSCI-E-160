da44f8f54903a8903352a799517aad8104621c4d
John Doyle <john.c.doyle@gmail.com>
2012-09-16 18:12:04 -0400

    Completed HW1

---
+++ src/cscie160/tw1/ElevatorTest.java
@@ -0,0 +1,30 @@
+package cscie160.tw1;
+
+import cscie160.hw1.Elevator;
+
+/**
+* Test the java class Elevator.
+* 
+* @author	John Doyle
+* @version	1.0
+*/
+public class ElevatorTest {
+
+	/**
+	* Add three passengers and run the elevator up and down the building to
+	* ensure the passengers get off correctly.
+	*
+	*/
+	public static void main(String [] args) {
+		Elevator myElevator = new Elevator();
+		myElevator.boardPassenger(2);
+		myElevator.boardPassenger(2);
+		myElevator.boardPassenger(3);
+		
+		System.out.println(myElevator);
+		
+		for(int i = 0; i < 12; i++) {
+			myElevator.move();
+		}
+	}	
+}
